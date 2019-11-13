b8a1b0384cd82a72016633b2e5c27a2acefcfab3
sfauvel <sfauvel@gmail.com>
2019-11-13 22:24:15 +0100

    Improve commit message demo

--- src/main/java/org/dojo/livingdoc/demo/GitLogMessage.java
+++ src/main/java/org/dojo/livingdoc/demo/GitLogMessage.java
@@ -1,16 +1,18 @@
 package org.dojo.livingdoc.demo;
 
 import org.dojo.livingdoc.annotation.ClassDemo;
+import org.dojo.livingdoc.annotation.GenerateDoc;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 
@@ -24,28 +26,39 @@ import java.util.stream.StreamSupport;
 @ClassDemo(group = "Changelog", label = "Extract changelog from git messages")
 public class GitLogMessage {
 
-    public final static File GIT_DIR = new File("../.git");
+    private final Path gitPath = Path.of(".git");
 
     public static void main(String[] args) throws IOException, GitAPIException {
 
+        GitLogMessage gitLogMessage = new GitLogMessage();
+
+        System.out.println(gitLogMessage.generateGitMessages());
+    }
+
+    @GenerateDoc(name = "Git history:")
+    // tag::example[]
+    public String generateGitMessages() throws IOException, GitAPIException {
         Repository repository = new FileRepositoryBuilder()
-                .setGitDir(GIT_DIR)
+                .setGitDir(gitPath.toFile())
                 .setMustExist(true)
                 .build();
 
-        Iterable<RevCommit> log = new Git(repository).log().call();
+        Iterable<RevCommit> logs = new Git(repository).log().call();
 
-        StreamSupport.stream(log.spliterator(), false)
-                .limit(100)
-                .map(rev ->  idAndMessage(rev, "dd/MM/yyyy"))
-                .forEach(System.out::println);
+        return StreamSupport.stream(logs.spliterator(), false)
+                .limit(10)
+                .map(rev -> formatMessage(rev, "dd/MM/yyyy"))
+                .collect(Collectors.joining("\n"));
     }
 
-
-    public static String idAndMessage(RevCommit rev, String dateFormat) {
+    public String formatMessage(RevCommit rev, String dateFormat) {
         Date authorDate = rev.getAuthorIdent().getWhen();
 
-        String format = new SimpleDateFormat(dateFormat).format(authorDate);
-        return rev.getName().substring(0, 10) + " " + format + ": " + rev.getShortMessage();
+        String dateFormatted = new SimpleDateFormat(dateFormat).format(authorDate);
+        return String.format("* *%s* (%s): %s",
+                dateFormatted,
+                rev.getName().substring(0, 10),
+                rev.getShortMessage());
     }
+    // end::example[]
 }
